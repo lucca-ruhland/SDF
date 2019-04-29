c6f7d0565f958e8049f589a68f2774f15ce89ca2
Lucca Ruhland <lucca.ruhland@gmx.net>
2019-04-29 23:42:11 +0200

    Added all plots to generator.py, besides vector products. Multiple animated plots are working.
    - multiple plots are working now
    - to do:
      -> vector product -> scalar product is not working because of the
         vector dimension => NEEDS FIX
      -> arange position of plots with subplot2grid
    - plots have stable performance now
    - animation is not repeating: solves issues with graphs drawing
    horizontal line when resetting frame ! possible fix: reseat data if
    frame = 0

--- generator.py
+++ generator.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.pylab import *
 import matplotlib.patches as mpatch
+from mpl_toolkits.axes_grid1 import host_subplot
 from matplotlib.animation import FuncAnimation
 
 class Aircraft:
@@ -67,22 +69,77 @@ class Aircraft:
         self.norm = self.get_normal(t)
 
 def init():
+    #set up ax
     ax.set_xlim(-12000, 12000)
     ax.set_ylim(-12000, 12000)
     ax.set_title("aircraft trajectory")
     ax.set_xlabel("y in meter")
     ax.set_ylabel("x in meter")
     ax.grid(True)
-    return ln, ln2,
+
+    #set up ax2
+    ax2.set_xlim(-10, 430)
+    ax2.set_ylim(0, 400)
+    ax2.set_title("aircraft velocity")
+    ax2.set_xlabel("r'(t) in meter")
+    ax2.set_ylabel("t in meter")
+    ax2.grid(True)
+
+    # set up ax3
+    ax3.set_xlim(-10, 430)
+    ax3.set_ylim(-2, 12)
+    ax3.set_title("aircraft acceleration")
+    ax3.set_xlabel("r''(t) in m/s²")
+    ax3.set_ylabel("t in meter")
+    ax3.grid(True)
+
+    # set up ax4
+    ax4.set_xlim(-10, 430)
+    ax4.set_ylim(-2, 12)
+    ax4.set_title("r''(t)t(t)")
+    ax4.set_xlabel("r''(t)t(t) in m/s²")
+    ax4.set_ylabel("t in meter")
+    ax4.grid(True)
+
+    # set up ax5
+    ax5.set_xlim(-10, 430)
+    ax5.set_ylim(-2, 12)
+    ax5.set_title("r''(t)n(t)")
+    ax5.set_xlabel("r''(t)n(t) in m/s²")
+    ax5.set_ylabel("t in meter")
+    ax5.grid(True)
+
+    return ln, ln2, ln3, ln4, ln5, ln6
 
 def update(frame):
+    # update values
     air.update_stats(frame)
+    # appending data for object trace
     xdata.append(air.position[0])
     ydata.append(air.position[1])
     ln.set_data(xdata, ydata)
+    # set up line for moving object
     ln2.set_data(air.position[0], air.position[1])
 
-    #plot vectors as quiver
+    # set up time array
+    time.append(frame)
+
+    # set up line for aircraft velocity
+    v.append(np.linalg.norm(air.velocity))
+    ln3.set_data(time, v)
+
+    # set up line for aircraft acceleration
+    q.append(np.linalg.norm(air.acceleration))
+    ln4.set_data(time, q)
+
+    # set up line for r''(t)t(t)
+    r_t.append(np.dot(air.acceleration,air.tang))
+    ln5.set_data(time, r_t)
+
+    # set up line for r''(t)n(t)
+
+    # plot vectors as quiver
+    # plot tangential vector
     Q1 = ax.quiver(air.position[0], air.position[1], air.tang[0], air.tang[1], units='xy',
                          scale=0.0005, color='g')
     qk1 = ax.quiverkey(Q1, 0.9, 0.9, 2, r'$tangential vector$', labelpos='E',
@@ -92,7 +149,7 @@ def update(frame):
                          scale=0.0005, color='y')
     qk2 = ax.quiverkey(Q1, 0.9, 0.9, 2, r'$normal vector$', labelpos='E',
                              coordinates='figure')
-    return ln, ln2, Q1, Q2,
+    return ln, ln2, ln3, ln4, ln5, Q1, Q2,
 
 def main():
   pass
@@ -101,11 +158,35 @@ if __name__ == "__main__":
     T = 419
     air = Aircraft(300, 9, 0)
 
-    fig, ax = plt.subplots()
+
+    font = {'size': 9}
+    matplotlib.rc('font', **font)
+    #fig, ax = plt.subplots()
+    fig = figure(num=0, figsize=(12, 8))  # , dpi = 100)
+    fig.suptitle("ground truth generator", fontsize=12)
+    ax = plt.subplot2grid((3, 3), (0, 0), colspan=2)
+    ax2 = plt.subplot2grid((3, 3), (1, 0), colspan=2)
+    ax3 = plt.subplot2grid((3, 3), (1, 2), rowspan=2)
+    ax4 = plt.subplot2grid((3, 3), (2, 0))
+    ax5 = plt.subplot2grid((3, 3), (2, 1))
+
+    #set up arrays to save data of plots
     xdata, ydata = [], []
-    ln, = plt.plot([], [])
-    ln2, = plt.plot([], [], 'ro')
+    v = []
+    q = []
+    r_t = []
+    r_n = []
+    time = []
+    # set up lines to plot
+    ln, = ax.plot([], [])
+    ln2, = ax.plot([], [], 'ro')
+    ln3, = ax2.plot([], [])
+    ln4, = ax3.plot([], [])
+    ln5, = ax4.plot([], [])
+    ln6, = ax5.plot([], [])
+    #plot animation
     ani = FuncAnimation(fig, update, frames=T,
-                        init_func=init, interval=10,  blit=True)
+                        init_func=init, interval=10,  blit=True, repeat=False)
+    plt.tight_layout()
     plt.show()
     #main()
--- ground_truth_generator.py
+++ ground_truth_generator.py
@@ -13,7 +13,8 @@ class Aircraft:
         self.velocity = np.zeros((2, 1))
         self.acceleration = np.zeros((2, 1))
         self.tang = np.zeros((2, 1))
-        self. norm = np.zeros((2, 1))
+        self.norm = np.zeros((2, 1))
+
 
     def get_w_a(self, t):
         A = (self.v ** 2) / self.q
@@ -46,15 +47,15 @@ class Aircraft:
     def get_tangential(self, t):
         tang = np.zeros((2, 1))
         r_i = self.get_velocity(t)
-        tang[0] = 1/(np.linalg.norm(r_i)) * r_i[0]
-        tang[1] = 1/(np.linalg.norm(r_i)) * r_i[1]
+        tang[0] = (1/(np.linalg.norm(r_i))) * r_i[0]
+        tang[1] = (1/(np.linalg.norm(r_i))) * r_i[1]
         return tang
 
     def get_normal(self, t):
         n = np.zeros((2, 1))
         r_i = self.get_velocity(t)
-        n[0] = 1 / (np.linalg.norm(r_i)) * -r_i[1]
-        n[1] = 1 / (np.linalg.norm(r_i)) * r_i[0]
+        n[0] = (1 / (np.linalg.norm(r_i))) * -r_i[1]
+        n[1] = (1 / (np.linalg.norm(r_i))) * r_i[0]
         return n
 
     def update_stats(self, t):
@@ -66,22 +67,21 @@ class Aircraft:
         self.norm = self.get_normal(t)
 
 class AnimatedPlot(object):
-    def __init__(self, ax1, ax2, ax3, ax4, air):
+    def __init__(self, ax1, ax2, air):
+        # init axes
         self.ax1 = ax1
         self.ax2 = ax2
-        self.ax3 = ax3
-        self.ax4 = ax4
         self.air = air
+
+        # set axis limits and labels
         self.ax1.set_xlim(-12000, 12000)
         self.ax1.set_ylim(-12000, 12000)
         self.ax1.set_xlabel("X in m")
         self.ax1.set_ylabel("Y in m")
         self.ax1.set_title("aircraft trajectory")
-        self.line, = ax1.plot([], [], 'bo')
 
-        self.ax2.set_title("aircraft velocity")
-        self.ax3.set_title("aircraft acceleration")
-        self.ax4.set_title("r''(t) * t(t) & r''(t) * n(t)")
+        # plot lines
+        self.line, = ax1.plot([], [], 'bo')
 
     def init(self):
         self.line.set_data([], [])
@@ -91,56 +91,32 @@ class AnimatedPlot(object):
         if i == 0:
             return self.init()
 
+        # get position of aircraft
         self.air.update_stats(i)
         x = self.air.position[0]
         y = self.air.position[1]
         self.line.set_data(x, y)
 
-        #plot vectors as quivers
-
-        #plot velocity:
-        Q1 = self.ax1.quiver(self.air.position[0], self.air.position[1], self.air.velocity[0], self.air.velocity[1], units='xy', scale=0.1)
-        qk1 = self.ax1.quiverkey(Q1, 0.9, 0.9, 2, r'$v: \frac{m}{s}$', labelpos='E',
+        # plot tangential vector
+        Q1 = self.ax1.quiver(self.air.position[0], self.air.position[1], self.air.tang[0], self.air.tang[1], units='xy', scale=0.0005, color='g')
+        qk1 = self.ax1.quiverkey(Q1, 0.9, 0.9, 2, r'$tangential vector$', labelpos='E',
+                                 coordinates='figure')
+        # plot normal vector
+        Q2 = self.ax1.quiver(self.air.position[0], self.air.position[1], self.air.norm[0], self.air.norm[1], units='xy', scale=0.0005, color='r')
+        qk2 = self.ax1.quiverkey(Q1, 0.9, 0.9, 2, r'$normal vector$', labelpos='E',
                                  coordinates='figure')
 
-        Q1_2 = self.ax2.quiver(np.array((0)), np.array((0)), self.air.velocity[0], self.air.velocity[1], units='xy', scale=10000)
-        qk1_2 = self.ax2.quiverkey(Q1, 0.9, 0.9, 2, r'$v: \frac{m}{s}$', labelpos='E',
-                                 coordinates='figure')
-        #plot acceleration:
-        Q2 = self.ax1.quiver(self.air.position[0], self.air.position[1], self.air.acceleration[0], self.air.acceleration[1], units='xy', scale=0.0025)
-        qk2 = self.ax1.quiverkey(Q1, 0.9, 0.9, 2, r'$q: \frac{m}{s^2}$', labelpos='E',
-                                 coordinates='figure')
-
-        Q2_2 = self.ax3.quiver(np.array((0)), np.array((0)), self.air.acceleration[0], self.air.acceleration[1], units='xy', scale=250)
-        qk2_2 = self.ax3.quiverkey(Q1, 0.9, 0.9, 2, r'$q: \frac{m}{s^2}$', labelpos='E',
-                                 coordinates='figure')
-
-        temp_1 = self.air.acceleration * self.air.tang
-
-        #plot r''(t) * t(t):
-        Q3 = self.ax4.quiver(np.array((0)), np.array((0)), temp_1[0], temp_1[1], units='xy', scale=250)
-        qk3 = self.ax4.quiverkey(Q3, 0.9, 0.9, 2, r'$r''(t) * t(t)$', labelpos='E',
-                                 coordinates='figure')
-
-
-        temp_2 = self.air.acceleration * self.air.norm
-
-        #plot r''(t) * norm(t):
-
-        Q4 = self.ax4.quiver(np.array((0)), np.array((0)), temp_2[0], temp_2[1], units='xy', scale=250)
-        qk4 = self.ax4.quiverkey(Q3, 0.9, 0.9, 2, r'$r''(t) * n(t)$', labelpos='E',
-                                   coordinates='figure')
-
-        return self.line, Q1, Q1_2, Q2, Q2_2, Q3, Q4
+        return self.line, Q1, Q2
 
 def main():
     # period T is 400/3 * Pi = 418.879 ...
     T = 419
     air = Aircraft(300, 9, 0)
     fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(nrows=2, ncols=2)
-    ap = AnimatedPlot(ax1, ax2, ax3, ax4, air)
+    ap = AnimatedPlot(ax1, air)
     anim = FuncAnimation(fig, ap, frames=T, init_func=ap.init,
                          interval=10, blit=True)
+
     plt.tight_layout()
     plt.show()
 
