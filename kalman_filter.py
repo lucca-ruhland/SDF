0efb63b9fad46ff4da33814bf7f068c03f08784c
Lucca Ruhland <lucca.ruhland@gmx.net>
2019-06-05 20:09:19 +0200

    Fix minor error in Kalman Filter prediction

--- kalman_filter.py
+++ kalman_filter.py
@@ -18,7 +18,8 @@ class kalman:
 
         # init local vars
         # state vector
-        self.x = np.zeros((4, 1))
+        # self.x = np.zeros((4, 1))
+        self.x = self.init_state_vecor(0)
         # self.R = self.sigma * np.eye(2)
         self.z = np.zeros((2, 1))
         # Covariance P of pdf
@@ -44,14 +45,19 @@ class kalman:
     def get_dynamics_f(self):
         return np.array([1, 0, self.delta_t, 0, 0, 1, 0, self.delta_t, 0, 0, 1, 0, 0, 0, 0, 1]).reshape((4, 4))
 
+    def init_state_vecor(self, t):
+        self.air.update_stats(t)
+        return np.array([self.air.position[0], self.air.position[1], self.air.velocity[0], self.air.velocity[1]]).reshape((4, 1))
+
     def prediction(self, t):
         """Returns a prediction based on a dynamic model of sensor measurements"""
         self.air.update_stats(t)
-        self.x = np.array([self.z[0], self.z[1], self.air.velocity[0], self.air.velocity[1]]).reshape(4, 1)
+        x = self.x
         F = self.get_dynamics_f()
         D = self.get_covariance_d()
-        self.x = np.dot(F, self.x)
-        self.P = np.dot(np.dot(F, self.P), F.T) + D
+        x = np.dot(F, x)
+        P = np.dot(np.dot(F, self.P), F.T) + D
+        return x, P
 
     def filtering(self, t):
         """Returns a filtered dynamic model"""
@@ -62,26 +68,28 @@ class kalman:
 
         S = np.dot(np.dot(H, self.P), H.T) + R
         W = np.dot(np.dot(self.P, H.T), np.linalg.inv(S))
-        self.x = self.x + np.dot(W, v)
-        self.P = self.P - np.dot(np.dot(W, S), W.T)
+        x = self.x + np.dot(W, v)
+        P = self.P - np.dot(np.dot(W, S), W.T)
+        return x, P
 
     def fusion(self, t):
-        self.z = np.zeros((2, 1))
+        z = np.zeros((2, 1))
         R = self.get_covariance_r()
         for arg in self.sensors:
             if isinstance(arg, Sensor):
                 _r = arg.sigma_c**2 * np.eye(2)
                 arg.update_stats(t)
                 _z = np.array([arg.z_c[0], arg.z_c[1]]).reshape(2, 1)
-                self.z = self.z + np.dot(np.linalg.inv(_r), _z)
-        self.z = np.matmul(R, self.z)
-        print("fusion:\n", self.z)
+                z = z + np.dot(np.linalg.inv(_r), _z)
+        z = np.matmul(R, z)
+        print("fusion:\n", z)
+        return z
 
 
     def update_cartesian(self, t):
-        self.prediction(t)
-        self.fusion(t)
-        self.filtering(t)
+        self.x, self.P = self.prediction(t)
+        self.z = self.fusion(t)
+        self.x, self.P = self.filtering(t)
 
 
 if __name__ == "__main__":
